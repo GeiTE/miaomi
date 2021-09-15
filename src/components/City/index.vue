e7cb3981613098595816cc6e0c73c970fb6f720b
GeiTE <yuhaoyuang@163.com>
2021-09-15 22:07:51 +0800

    数据渲染

---
+++ src/components/City/index.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="city_body">
+        <div class="city_list">
+            <div class="city_sort" ref="city_sort">
+                <div v-for="item in cityList" :key="item.index">
+                    <h2>{{item.index}}</h2>
+                    <ul>
+                        <li v-for="itemList in item.list" :key="itemList.id">
+                            {{itemList.nm}}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="city_index">
+            <ul>
+                <li v-for="(item,index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{item.index}}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'City',
+    data() {
+        return {
+            cityList : []
+        }
+    },
+    mounted() {
+        this.axios.get('/dianying/cities.json').then((res)=>{
+            var msg = res.statusText;
+            if(msg === "OK") {
+                var data = res.data.cts;
+                var cityList = this.formatCityList(data);
+                this.cityList = cityList;
+            }
+        });
+    },
+    methods : {
+        formatCityList(cities) {
+            var cityList = [];
+
+            for(var i = 0; i < cities.length; i++) {
+                var firstLetter = cities[i].py.substring(0,1);
+                if(toCom(firstLetter)) {      //新添加索引
+                    cityList.push({ index : firstLetter, list : [ { nm : cities[i].nm , id : cities[i].id } ] });
+                } else {                       //累加到已有索引
+                    for(var j = 0; j < cityList.length; j++) {
+                        if( cityList[j].index === firstLetter ) {
+                            cityList[j].list.push( { nm : cities[i].nm, id : cities[i].id } )
+                        }
+                    }
+                }
+            }
+
+            cityList.sort((n1, n2) => {
+                if( n1.index > n2.index ) {
+                    return 1;
+                } else if( n1.index < n2.index ) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            })
+
+            function toCom(firstLetter) {
+                for(var i = 0; i < cityList.length; i++) {
+                    if( cityList[i].index == firstLetter ) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return cityList;
+
+        },
+        handleToIndex(index) {
+            var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+            this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+        }
+    }
+}
+</script>
+
+<style scoped>
+#content .city_body{ margin-top: 45px; display: flex; width:100%; position: absolute; top: 0; bottom: 0;}
+.city_body .city_list{ flex:1; overflow: auto; background: #FFF5F0;}
+.city_body .city_list::-webkit-scrollbar{
+    background-color:transparent;
+    width:0;
+}
+.city_body .city_sort{}
+.city_body .city_sort div{ margin-top: 20px;}
+.city_body .city_sort h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;text-transform : uppercase;}
+.city_body .city_sort ul{ padding-left: 10px; margin-top: 10px;}
+.city_body .city_sort ul li{ line-height: 30px; line-height: 30px;}
+.city_body .city_index{ width:20px; display: flex; flex-direction:column; justify-content:center; text-align: center; border-left:1px #e6e6e6 solid;text-transform : uppercase;}
+</style>
